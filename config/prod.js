4a9bd2ef488e65914f06932a4bfb624f028ee890
diego <dpcalvo@gmail.com>
2018-04-16 17:48:26 -0500

    Keys modify

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '765730993931-rlce0q7j6uhhofn27uhkg7h94vdsem4i.apps.googleusercontent.com',
+    googleClientSecret: 'SWRhvi0iClnR-EEVhCrzCvui',
+    mongoURI: 'mongodb://diego:feedy123_@ds139919.mlab.com:39919/feedy-dev',
+    cookieKey: 'ihgfjasdhfagasudasdmcolqllwkeaosdifcmnzzzza'
+};
