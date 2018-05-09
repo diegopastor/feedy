7ac2e8c39d23e51a0e0ec590a2169e1ea7ae7931
diego <dpcalvo@gmail.com>
2018-05-08 21:12:34 -0500

    Add header and login logout flow

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
+  renderContent(){
+    switch (this.props.auth){
+      case null:
+        return;
+      case false:
+        return (
+          <li><a href="/auth/google">Login With Google</a></li>
+          );
+      default: 
+        return (
+          <li><a href="/api/logout">Logout</a></li>
+          );
+    }
+  }
+
 	render(){
 		return(
 		<nav>
-    		<div class="nav-wrapper">
-      			<a href="#" class="left brand-logo">Feedy</a>
-      			<ul id="nav-mobile" class="right hide-on-med-and-down">
-        			<li><a>Login With Google</a></li>
+    		<div className="nav-wrapper">
+      			<Link 
+            to={this.props.auth ? '/surveys' : '/'} 
+            className="left brand-logo"
+            >
+            Feedy
+            </Link>
+      			<ul id="nav-mobile" className="right hide-on-med-and-down">
+        			{ this.renderContent() }
       			</ul>
     		</div>
   		</nav>
@@ -15,4 +37,8 @@ class Header extends Component {
 	}
 } 
 
-export default Header;
+function mapStateToProps({ auth }){
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
